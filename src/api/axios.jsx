91582a5e0b3105988edc3a1196da28683c6d487d
HassanTariqAlvi <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-03-26 21:15:49 +0500

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:8000/api/";
-// const BASE_URL = "https://rubbani-industry-backend.herokuapp.com/api/";
-// const BASE_URL = "https://muddassirtahiri.pythonanywhere.com/api/";
 export default axios.create({
   baseURL: BASE_URL,
 });
